4d3947ba74c2b3d0c2c9d8829172b64fddae725c
Aron Badics <badar@badar-thinkpad.localdomain>
2012-01-18 23:48:26 +0100

    minor changes

--- src/bomberman.cpp
+++ src/bomberman.cpp
@@ -1,13 +1,19 @@
 #include "bomberman.hpp"
 
-Bomberman::Bomberman() : is_running(true)
-	{
-	}
+Bomberman::Bomberman() : is_running(true){}
 
 int Bomberman::start(const std::vector<CL_String> &args)
 	{
+		window=new CL_DisplayWindow("Bomberman", 800, 600, false, true);
+		gc=&window->get_gc();
+		CL_ResourceManager resources;
+		CL_Slot slot_quit = window->sig_window_close().connect(this, &Bomberman::on_quit);
+		
 		// main loop
 		while(is_running)
 			{
+				window->flip();
+				CL_KeepAlive::process();
+				CL_System::sleep(10);
 			}
 	}
--- src/bomberman.hpp
+++ src/bomberman.hpp
@@ -7,10 +7,14 @@ class Bomberman{
 
 private:
 	bool is_running;
+	CL_GraphicContext *gc;
+	CL_DisplayWindow *window;
+	CL_ResourceManager *recourses;
 
 public:
 	Bomberman();
 	int start(const std::vector<CL_String> &args);
+	void on_quit()	{is_running=false;}
 };
 
 #endif //BOMBERMAN_HPP
